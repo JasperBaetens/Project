a93d2b41d5d970bbc0b936c2bb48b9c3b448187f
JasperBaetens <jasper.baetens@student.kdg.be>
2015-01-16 01:46:33 +0100

    Responsive confirmation

--- Sonar/confirmation.php
+++ Sonar/confirmation.php
@@ -39,35 +39,21 @@
         <div class="confirm">
             <div class="components">
                 <p class="your">Your</p>
-                <a href="http://localhost/project/Sonar/"><img src="img/LOGO-pink.png" alt="Sonario" class="logo"></a>
+                <a href="http://localhost/project/Sonar/"><img src="img/LOGO-pink.png" alt="Sonario" class="logo-3"></a>
                 <p>Has been created!</p>
 
                 <div class="tweet">
                 <form autocomplete="off">
-                    <p>It's name is '<?php if(isset($_POST["name"])){echo $_POST["name"]} ?>'</p>
+                <div class="tweet-wrapper">
+                    <p>It's name is '
+                    
+                    '</p>
                     <p>Tweet it to <input type="text" name="tweet" id="tweet" placeholder="............" value="" /> </p>
                     </br>
                     
                     
                     <a href="https://twitter.com/intent/tweet?screen_name=NaneBoudewijns&text=sonario" id="tweet-btn" data-related="">Tweet</a>
-                        <script>
-                            var tweet = document.getElementById("tweet");
-                            
-                            var btn = document.getElementById("tweet-btn");
-                            
-                            
-                            
-                            function useValue() {
-                                var val = tweet.value;
-                                btn.setAttribute("href", 'https://twitter.com/intent/tweet?screen_name='+val+'&text=+I%20have%20made%20a%20sonario%20fo%20you:%20+<?php echo $_POST["name"] ?>+%20%23sonario');
-                            }
-                            tweet.onchange = useValue;  
-                            
-                            
-                            
-                            
-                            !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id))                  {js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
-                    </script>
+                </div>
                 </form>
                 </div>
             </div> 
@@ -81,28 +67,3 @@
 </body>
 </html>
 
-<?php
-
-var_dump($_POST);
-
-            $db = new PDO('mysql:host=localhost;dbname=dbsonario;charset=utf8', 'root', '');
-
-            /*$statement = $db->prepare("SELECT lat FROM Sonario WHERE name='myplace' LIMIT 1");
-            $statement->execute();
-            $row = $statement->fetch(); */
-
-            //echo $row['lat'];
-
-            //var_dump($_POST);
-            if(isset($_REQUEST['foo']))
-            {
-                $insert = $db->prepare("INSERT INTO sonario(name, longit, lat, radius) VALUES(:fname, :flongit, :flat, :frad)");
-                $insert->execute(array(
-                    "fname" => $_POST["name"],
-                    "flongit" => $_POST["long"],
-                    "flat" => $_POST["lat"],
-                    "frad" => $_POST["range"],
-                ));
-            }
-
-            ?>
